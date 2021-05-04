ac415a647999b2d19da081211fee46e2897aeb77
dhchun1203 <dhchun1203@gmail.com>
2021-05-04 09:18:58 +0900

    Page structure rebuild

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+let imgBtn = document.querySelector(".imageBtn");
+console.log(imgBtn);
+class PageComponent {
+    constructor() {
+        this.content = document.querySelector(".content");
+        this.a = document.createElement("p");
+    }
+}
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,7 @@
+let imgBtn = document.querySelector<HTMLButtonElement>(".imageBtn")!;
+console.log(imgBtn);
+
+class PageComponent {
+	content = document.querySelector(".content")!;
+	a = document.createElement("p");
+}
