8f1ac46f24c46807b4d368b87126ef16e9f185ef
dhchun1203 <dhchun1203@gmail.com>
2021-05-21 18:27:18 +0900

    Container Component: PageComponent

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
-import { PageComponent } from "./components/page/page.js";
+import { Composable, PageComponent } from "./components/page/page.js";
 import { ImageComponent } from "./components/page/item/image.js";
 import { VideoComponent } from "./components/page/item/video.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { TodoComponent } from "./components/page/item/todo.js";
+import { Component } from "./components/component.js";
 class App {
-	private readonly page: PageComponent;
+	private readonly page: Component & Composable;
 	constructor(appRoot: HTMLElement) {
 		this.page = new PageComponent();
 		this.page.attachTo(appRoot);
@@ -13,19 +14,29 @@ class App {
 			"image title",
 			"https://picsum.photos/600/300"
 		);
-		image.attachTo(appRoot, "beforeend");
+		// image.attachTo(appRoot, "beforeend");
+		this.page.addChild(image);
 
 		const video = new VideoComponent(
 			"healing music",
 			"https://youtu.be/ArQvRDWulns"
 		);
-		video.attachTo(appRoot, "beforeend");
+		// video.attachTo(appRoot, "beforeend");
+		this.page.addChild(video);
+		const video2 = new VideoComponent(
+			"healing music",
+			"https://youtu.be/oXz0jLFC69Y"
+		);
+		// video2.attachTo(appRoot, "beforeend");
+		this.page.addChild(video2);
 
 		const note = new NoteComponent("Hey!", "hello!");
-		note.attachTo(appRoot, "beforeend");
+		// note.attachTo(appRoot, "beforeend");
+		this.page.addChild(note);
 
 		const todo = new TodoComponent("Todo", "netflix");
-		todo.attachTo(appRoot, "beforeend");
+		// todo.attachTo(appRoot, "beforeend");
+		this.page.addChild(todo);
 	}
 }
 
